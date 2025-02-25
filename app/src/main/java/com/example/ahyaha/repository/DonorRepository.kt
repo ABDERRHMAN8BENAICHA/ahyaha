97ff73b656747e545b8d95c3c057ce1c15cbc78e
abdelmouiz <mehellouabdelmouiz9@gmail.com>
2025-02-25 22:10:12 +0100

    tp2 done

--- app/src/main/java/com/example/ahyaha/repository/DonorRepository.kt
+++ app/src/main/java/com/example/ahyaha/repository/DonorRepository.kt
@@ -4,16 +4,15 @@ import com.example.ahyaha.model.Donor
 import java.util.Date
 
 object DonorRepository {
-    
+
     fun getAllDonors(): List<Donor> {
-        // Simulating fetching data from a data source
         return listOf(
             Donor(
                 id = "1",
                 name = "John Doe",
                 email = "john.doe@example.com",
                 phoneNumber = "1234567890",
-                profilePicture = "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+                profilePicture = "https://randomuser.me/api/portraits/men/1.jpg",
                 bloodGroup = "A",
                 Rh = "+",
                 location = "New York",
@@ -26,7 +25,7 @@ object DonorRepository {
                 name = "Jane Smith",
                 email = "jane.smith@example.com",
                 phoneNumber = "0987654321",
-                profilePicture = "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+                profilePicture = "https://randomuser.me/api/portraits/women/2.jpg",
                 bloodGroup = "B",
                 Rh = "-",
                 location = "Los Angeles",
@@ -36,11 +35,24 @@ object DonorRepository {
             ),
             Donor(
                 id = "3",
-                name = "Michael Johnson",
-                email = "michael.johnson@example.com",
+                name = "Walter White",
+                email = "walter.white@example.com",
                 phoneNumber = "5555555555",
-                profilePicture = "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+                profilePicture = "https://randomuser.me/api/portraits/men/3.jpg",
                 bloodGroup = "O",
+                Rh = "+",
+                location = "Albuquerque",
+                lastDonationDate = Date(),
+                createdAt = Date(),
+                updatedAt = Date()
+            ),
+            Donor(
+                id = "4",
+                name = "Trevor Turner",
+                email = "trevor.turner@example.com",
+                phoneNumber = "4444444444",
+                profilePicture = "https://randomuser.me/api/portraits/men/4.jpg",
+                bloodGroup = "AB",
                 Rh = "+",
                 location = "Chicago",
                 lastDonationDate = Date(),
@@ -48,31 +60,57 @@ object DonorRepository {
                 updatedAt = Date()
             ),
             Donor(
-                id = "4",
-                name = "Emily Davis",
-                email = "emily.davis@example.com",
-                phoneNumber = "4444444444",
-                profilePicture = "https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-                bloodGroup = "AB",
-                Rh = "+",
+                id = "5",
+                name = "Alexander Brown",
+                email = "alex.brown@example.com",
+                phoneNumber = "3333333333",
+                profilePicture = "https://randomuser.me/api/portraits/men/5.jpg",
+                bloodGroup = "A",
+                Rh = "-",
                 location = "Houston",
                 lastDonationDate = Date(),
                 createdAt = Date(),
                 updatedAt = Date()
             ),
             Donor(
-                id = "5",
-                name = "David Wilson",
-                email = "david.wilson@example.com",
-                phoneNumber = "3333333333",
-                profilePicture = "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-                bloodGroup = "A",
-                Rh = "-",
+                id = "6",
+                name = "Emily Davis",
+                email = "emily.davis@example.com",
+                phoneNumber = "2222222222",
+                profilePicture = "https://randomuser.me/api/portraits/women/6.jpg",
+                bloodGroup = "B",
+                Rh = "+",
                 location = "Phoenix",
                 lastDonationDate = Date(),
                 createdAt = Date(),
                 updatedAt = Date()
-            )
+            ),
+            Donor(
+                id = "7",
+                name = "Michael Johnson",
+                email = "michael.johnson@example.com",
+                phoneNumber = "1111111111",
+                profilePicture = "https://randomuser.me/api/portraits/men/7.jpg",
+                bloodGroup = "O",
+                Rh = "-",
+                location = "San Francisco",
+                lastDonationDate = Date(),
+                createdAt = Date(),
+                updatedAt = Date()
+            ),
+            Donor(
+                id = "8",
+                name = "jack daniel ",
+                email = "emily.davis@example.com",
+                phoneNumber = "2222222222",
+                profilePicture = "https://www.utopix.com/fr/blog/wp-content/uploads/2024/04/OGI0Mzc2YjItMTVlNS00MzBjLThmOWUtMmM0ZDIxZDhkYjA2_1cbeaae0-7898-4972-bec2-818e8b74357b_profilhomme6-scaled.jpg",
+                bloodGroup = "B",
+                Rh = "+",
+                location = "Phoenix",
+                lastDonationDate = Date(),
+                createdAt = Date(),
+                updatedAt = Date()
+            ),
         )
     }
 }
