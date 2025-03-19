9032e086f7f3a0ac44223715affb2588f28ea8b3
Ali Yamani <aliyamani1956@gmail.com>
2025-03-19 09:35:14 +0100

    tp3 done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
 
     //Dagger Hilt
     implementation(libs.dagger.hilt)
+    implementation(libs.androidx.ui.android)
     kapt(libs.dagger.hilt.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
 
