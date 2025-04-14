ed503e992792f430d2853494a0c73f6770d8b65c
Ahmed <ahmed.g@turing.com>
2025-04-14 11:47:28 +0100

    Add the Final Task Description

--- app/src/main/java/com/example/ahyaha/domain/usecase/GetDonorsUseCase.kt
+++ app/src/main/java/com/example/ahyaha/domain/usecase/GetDonorsUseCase.kt
@@ -4,5 +4,7 @@ import com.example.ahyaha.data.repository.DonorRepository
 import javax.inject.Inject
 
 class GetDonorsUseCase @Inject constructor(private val donorRepository: DonorRepository) {
+
+
     operator fun invoke() = donorRepository.getAllDonors()
 }
